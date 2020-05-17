9616b1988e45ebac49e9d9ad1ab903414dac4038
DTAX-01 <yolandayudina@gmail.com>
2020-05-17 14:19:51 +0700

    first kali

--- login.php
+++ login.php
@@ -10,6 +10,7 @@
     <form action="api/login.php" method="POST">
     Username: <input type="text" name="username"><br/>
     Password: <input type="text" name="password"><br/>
+    <!-- Password: <input type="text" name="password"><br/> -->
     <input type="submit">
     </form>
 </body>
